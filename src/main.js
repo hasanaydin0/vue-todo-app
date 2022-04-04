9ed196b4859532aebac975712913fdb156235073
hasanaydin0 <hasanaydin5243@gmail.com>
2022-04-04 10:16:17 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
 
-createApp(App).use(store).mount('#app')
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+createApp(App).use(store).mount("#app");
